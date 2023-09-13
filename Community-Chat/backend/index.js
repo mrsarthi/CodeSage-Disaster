18935e7c4fc134e3695deccfc619764e461381a7
Parth Sarthi Mishra <90957276+mrsarthi@users.noreply.github.com>
2023-09-13 09:17:13 +0530

    a few changes

--- Community-Chat/backend/index.js
+++ Community-Chat/backend/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const { default: axios } = require("axios");
 
 const app = express();
 app.use(express.json());
@@ -7,6 +8,23 @@ app.use(cors({ origin: true }));
 
 app.post("/authenticate", async (req, res) => {
     const { username } = req.body;
+
+
+    try {
+        const r = await axios.put(
+            'https://api.chatengine.io/users/',
+            { username: username, secret: username, first_name: username },
+            {
+                headers: {
+                    "private-key":
+                        "fb0198e0-ffa4-4a90-abaf-199ee31ecae5"
+                }
+            }
+        )
+    } catch (e) {
+
+    }
+
     return res.json({ username: username, secret: "sha256..." });
 });
 
