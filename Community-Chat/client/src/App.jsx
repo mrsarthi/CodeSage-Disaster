97ba656aa59d1ee44e472079adb1c4e28d69dc87
Parth Sarthi Mishra <90957276+mrsarthi@users.noreply.github.com>
2023-09-22 10:18:40 +0530

    new

---
+++ Community-Chat/client/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StreamChat } from 'stream-chat';
+import { Chat } from 'stream-chat-react';
+import Cookies from 'universal-cookie';
+
+const App = () => {
+    return (
+        <div>
+            <h1>Disaster</h1>
+        </div>
+    )
+}
+
+export default App
